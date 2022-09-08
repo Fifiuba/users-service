3eb4590930877287ed6f25bdd2d932b47a1210c2
alejovillores <avillores@fi.uba.ar>
2022-09-08 09:05:12 -0300

    feat: Adding coverage codecov

--- tests/user_controller_test.py
+++ tests/user_controller_test.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/users/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == [{"username": "Rick"}, {"username": "Morty"}]
