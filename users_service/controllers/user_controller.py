fb48583ed9473fcd159cf0a96f37b8b0072022b0
alejovillores <avillores@fi.uba.ar>
2022-09-09 11:50:34 -0300

    fix: Fix controller

--- users_service/controllers/user_controller.py
+++ users_service/controllers/user_controller.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
-from fastapi_utils.cbv import cbv
+
 
 user_router = APIRouter()
 
 
-@cbv(user_router)
-class UserController:
-    @user_router.get("/users/", tags=["users"])
-    def read_users():
-        return [{"username": "Rick"}, {"username": "Morty"}]
+@user_router.get("/users/", tags=["users"])
+def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
