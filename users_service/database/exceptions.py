3cc5fc31f5bf71bd70ccdb3e0009f464d920c971
agustinaa235 <asegura@fi.uba.ar>
2022-09-10 19:08:43 -0300

    Fix: problems with exceptions

--- users_service/database/exceptions.py
+++ users_service/database/exceptions.py
@@ -4,4 +4,14 @@ class UserInfoException(Exception):
 class UserAlreadyExists(UserInfoException):
     def __init__(self):
         self.status_code = 409  # conflic
-        self.details = "The user already exists"
+        self.detail = "The user already exists"
+
+class PassengerNotFoundError(UserInfoException):
+    def __init__(self):
+        self.status_code = 404  # conflic
+        self.detail = "The passenger does not exists"
+
+class DriverNotFoundError(UserInfoException):
+    def __init__(self):
+        self.status_code = 404  # conflic
+        self.detail = "The driver does not exists"
