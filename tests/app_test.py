8ebb50a24791f04f20f470fd6cbc44b68c0823ea
solfonte <msfontenla@fi.uba.ar>
2022-09-12 18:06:41 -0300

    feat: added tests for passenger creation

--- tests/app_test.py
+++ tests/app_test.py
@@ -28,17 +28,8 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 def registerClient():
-    
-    return response 
-
-def test_has_table():
-    assert sa.inspect(engine).has_table("users")
-    assert sa.inspect(engine).has_table("passengers")
-    assert sa.inspect(engine).has_table("drivers")
-
-"""def test_whenCreatingUserWithNotRegisteredName_createsTheUserCorrectly():
     response = client.post(
-        "/createUser",
+        "users/passenger/create",
         json={
             "name": "Sol",
             "password": "87654321",
@@ -46,23 +37,31 @@ def test_has_table():
             "age": 22,
         },
     )
+    return response 
+
+def test_has_table():
+    assert sa.inspect(engine).has_table("users")
+    assert sa.inspect(engine).has_table("passengers")
+    assert sa.inspect(engine).has_table("drivers")
+
+def test_whenCreatingAPassengerWithNotRegisteredName_createsTheUserCorrectly():
+    response = registerClient()
 
     assert response.status_code == status.HTTP_201_CREATED, response.text
     data = response.json()
     assert data["name"] == "Sol"
     # check de la hashed password?
-    assert data["password"] == "87654321"
     assert data["phone_number"] == "12345678"
     assert data["age"] == 22
     assert "id" in data
 
     # faltaria chequear que el id devuelve al user correcto
 
-def test_whenCreatingUserWithRegisteredName_doesNotcreateTheUser():
+def test_whenCreatingAPassengerWithRegisteredName_doesNotcreateThePassenger():
     registerClient()
     response = registerClient()
 
     assert response.status_code == status.HTTP_409_CONFLICT, response.text
     data = response.json()
-    assert data["detail"] == "The passenger already exists""""
-    
+    assert data["detail"] == "The passenger already exists"
+
