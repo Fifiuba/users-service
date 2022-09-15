de238ceed4deb179e99f1f8dcb1be6b726c3ff65
alejovillores <avillores@fi.uba.ar>
2022-09-15 14:26:39 -0300

    Fix CORS

--- users_service/app.py
+++ users_service/app.py
@@ -5,6 +5,8 @@ from .controllers import user_controller
 from .database.database import engine
 from .database import models
 
+app = FastAPI()
+
 origins = [
 
     # Cambiar por api-gateway
@@ -17,11 +19,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-
-
-app = FastAPI()
 app.include_router(user_controller.user_router, prefix="/users", tags=["Users"])
 
 
