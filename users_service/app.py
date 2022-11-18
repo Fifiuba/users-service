d5c63f0db8e15db31d0f7fa7a0ec67e7b8f58178
Franco Gomez <fdgomez@fi.uba.ar>
2022-11-18 15:26:40 -0300

    fix: move example event from app to main

--- users_service/app.py
+++ users_service/app.py
@@ -3,34 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .controllers import user_controller
 from .database.crud import logger
 
-
-# Post events
-
-from datadog_api_client import ApiClient, Configuration
-from datadog_api_client.v1.api.events_api import EventsApi
-from datadog_api_client.v1.model.event_create_request import EventCreateRequest
-
-body = EventCreateRequest(
-    title="Starting server",
-    text="User service starting",
-    priority="low",
-    alert_type="info",
-    host="users-service",
-    tags=[
-        "test:Example",
-    ],
-)
-
-configuration = Configuration()
-with ApiClient(configuration) as api_client:
-    api_instance = EventsApi(api_client)
-    response = api_instance.create_event(body=body)
-
-    print(response)
-
 logger.info("Starting server")
-
-########
 
 app = FastAPI()
 
