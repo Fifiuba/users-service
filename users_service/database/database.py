87ef976b1d0581bb636452081a3b7717bab89ac1
agustina235 <asegura@fi.uba.ar>
2022-12-13 18:51:24 -0300

    Feat: read database info from .env

--- users_service/database/database.py
+++ users_service/database/database.py
@@ -1,17 +1,21 @@
 # flake8: noqa
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from users_service.database.models import Base
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
 
 
 def init_database():
+    DB_URL = os.getenv("DATABASE_URL")
     engine = create_engine(
-        "postgresql://postgres:postgres@postgres:5432/users", echo=True
+        DB_URL, echo=True
     )
     global SessionLocal
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    # Base.metadata.drop_all(engine)
+
     Base.metadata.create_all(engine)
 
 
