cf7e99b2fc43d6980edf9e779a497dbe47410e70
agustina235 <asegura@fi.uba.ar>
2022-12-13 18:51:09 -0300

    Feat: read token info from .env

--- users_service/utils/token_handler.py
+++ users_service/utils/token_handler.py
@@ -1,8 +1,8 @@
 import os
 from jose import jwt
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 if "RUN_ENV" in os.environ.keys() and os.environ["RUN_ENV"] == "test":
     JWT_SECRET_KEY = "testcase"
@@ -16,14 +16,12 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 5
 
 def create_access_token(user_id: int, rol: str) -> str:
 
-    # expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
     to_encode = {"id": user_id, "rol": rol}
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
 
 def decode_token(token: str):
+    
     decoded_jwt = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
-
     return decoded_jwt
